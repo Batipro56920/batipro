96cd51710b82d35a62c1bbcefdbff2f6137f29d4
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-06-07 07:18:58 +0200

    Autorise les décimales françaises dans CurrencyInput

--- src/components/forms/CurrencyInput.tsx
+++ src/components/forms/CurrencyInput.tsx
@@ -2,5 +2,5 @@ import type { InputHTMLAttributes } from "react";
 import { TextInput } from "./TextInput";
 
 export function CurrencyInput(props: Omit<InputHTMLAttributes<HTMLInputElement>, "type">) {
-  return <TextInput type="number" inputMode="decimal" step="0.01" {...props} />;
+  return <TextInput inputMode="decimal" {...props} />;
 }
