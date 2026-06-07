e109ecc1fe8ee230c758c711a24f6e3d387cf126
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-06-07 07:19:06 +0200

    Autorise les décimales françaises dans PercentInput

--- src/components/forms/PercentInput.tsx
+++ src/components/forms/PercentInput.tsx
@@ -2,5 +2,5 @@ import type { InputHTMLAttributes } from "react";
 import { TextInput } from "./TextInput";
 
 export function PercentInput(props: Omit<InputHTMLAttributes<HTMLInputElement>, "type">) {
-  return <TextInput type="number" inputMode="decimal" step="0.01" min="0" max="100" {...props} />;
+  return <TextInput inputMode="decimal" {...props} />;
 }
