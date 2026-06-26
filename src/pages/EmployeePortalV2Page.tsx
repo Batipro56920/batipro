ac667884b5a68d494401cce62237e1adcc66b6f3
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-06-26 21:11:10 +0200

    Wire daily checklist into employee portal

--- src/pages/EmployeePortalV2Page.tsx
+++ src/pages/EmployeePortalV2Page.tsx
@@ -1,1 +1,11 @@
-export { default } from "./EmployeePortalTerrainV2Page";
+import EmployeeDailyChecklistWidget from "./EmployeeDailyChecklistWidget";
+import EmployeePortalTerrainV2Page from "./EmployeePortalTerrainV2Page";
+
+export default function EmployeePortalV2Page() {
+  return (
+    <>
+      <EmployeePortalTerrainV2Page />
+      <EmployeeDailyChecklistWidget />
+    </>
+  );
+}
