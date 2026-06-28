1c29cc33f404d997e92a130434828db82acffda3
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-06-28 10:54:10 +0200

    Export stock tracking panel

--- src/features/purchase-orders/index.ts
+++ src/features/purchase-orders/index.ts
@@ -2,5 +2,6 @@ export * from "./application/purchaseOrderFactory";
 export * from "./components/PurchaseOrderEditor";
 export * from "./components/PurchaseOrdersPanel";
 export * from "./components/PurchaseOrderStatusBadge";
+export * from "./components/StockTrackingPanel";
 export * from "./domain/types";
 export * from "./infrastructure/purchaseOrderRepository";
