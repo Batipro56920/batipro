8fdb1d0c8cf9fa57c19688a599248063095db7a1
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-06-30 16:10:12 +0200

    Surface terrain feedback alerts in chantier list types

--- src/features/chantiers/list/types/index.ts
+++ src/features/chantiers/list/types/index.ts
@@ -10,6 +10,8 @@ export type ChantierDerived = ChantierRow & {
   budgetHt: number | null;
   timeRatio: number | null;
   estimatedMargin: number | null;
+  terrainFeedbackOpenCount: number;
+  terrainFeedbackPriorityCount: number;
 };
 
 export type ChantierListFilters = {
