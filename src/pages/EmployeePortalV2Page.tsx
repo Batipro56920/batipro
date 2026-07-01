647e1f939306e25956ad3e10e2e73624b2e74719
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-01 22:21:46 +0200

    Wire employee field data widget into portal

--- src/pages/EmployeePortalV2Page.tsx
+++ src/pages/EmployeePortalV2Page.tsx
@@ -1,10 +1,12 @@
 import EmployeeDailyChecklistWidget from "./EmployeeDailyChecklistWidget";
+import EmployeeFieldDataWidget from "./EmployeeFieldDataWidget";
 import EmployeePortalTerrainV2Page from "./EmployeePortalTerrainV2Page";
 
 export default function EmployeePortalV2Page() {
   return (
     <>
       <EmployeePortalTerrainV2Page />
+      <EmployeeFieldDataWidget />
       <EmployeeDailyChecklistWidget />
     </>
   );
