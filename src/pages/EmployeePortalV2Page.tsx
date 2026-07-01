b1f17e855f36f054cf1ace1cb5409b6c1574f7fd
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-01 22:49:32 +0200

    Remove misplaced field data widget from employee portal

--- src/pages/EmployeePortalV2Page.tsx
+++ src/pages/EmployeePortalV2Page.tsx
@@ -1,12 +1,10 @@
 import EmployeeDailyChecklistWidget from "./EmployeeDailyChecklistWidget";
-import EmployeeFieldDataWidget from "./EmployeeFieldDataWidget";
 import EmployeePortalTerrainV2Page from "./EmployeePortalTerrainV2Page";
 
 export default function EmployeePortalV2Page() {
   return (
     <>
       <EmployeePortalTerrainV2Page />
-      <EmployeeFieldDataWidget />
       <EmployeeDailyChecklistWidget />
     </>
   );
