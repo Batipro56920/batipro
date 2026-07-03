8ea4e237f594b8218eef6b3996a833a0d78f8fd8
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-03 13:07:20 +0200

    Enable task template product autofill

--- src/features/product-catalog/index.ts
+++ src/features/product-catalog/index.ts
@@ -1,2 +1,6 @@
 export * from "./domain/types";
 export * from "./infrastructure/productCatalogRepository";
+
+import { installTaskTemplateProductAutofillBridge } from "./services/taskTemplateProductAutofillBridge";
+
+installTaskTemplateProductAutofillBridge();
