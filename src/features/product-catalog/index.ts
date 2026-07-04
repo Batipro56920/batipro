f0d4cc0a3f5309f3f031da14736d7856e314d822
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-04 13:40:57 +0200

    fix: re-enable stable Coco template organization

--- src/features/product-catalog/index.ts
+++ src/features/product-catalog/index.ts
@@ -1,5 +1,6 @@
 import { installProductDrawerPricingBridge } from "./services/productDrawerPricingBridge";
 import { installProductProcedureDisplayBridge } from "./services/productProcedureDisplayBridge";
+import { installTaskTemplateCocoAssistantBridge } from "./services/taskTemplateCocoAssistantBridge";
 import { installTaskTemplateLotSettingsBridge } from "./services/taskTemplateLotSettingsBridge";
 import { installTaskTemplateProductAutofillBridge } from "./services/taskTemplateProductAutofillBridge";
 
@@ -8,5 +9,6 @@ export * from "./infrastructure/productCatalogRepository";
 
 installProductDrawerPricingBridge();
 installProductProcedureDisplayBridge();
+installTaskTemplateCocoAssistantBridge();
 installTaskTemplateLotSettingsBridge();
 installTaskTemplateProductAutofillBridge();
