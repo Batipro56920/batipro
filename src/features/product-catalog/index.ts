87b53f80840ee1d651a24bf03eee98c3750e9b35
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-04 11:54:55 +0200

    Enable task lot settings bridge

--- src/features/product-catalog/index.ts
+++ src/features/product-catalog/index.ts
@@ -1,6 +1,7 @@
 import { installProductDrawerPricingBridge } from "./services/productDrawerPricingBridge";
 import { installProductProcedureDisplayBridge } from "./services/productProcedureDisplayBridge";
 import { installTaskTemplateCocoAssistantBridge } from "./services/taskTemplateCocoAssistantBridge";
+import { installTaskTemplateLotSettingsBridge } from "./services/taskTemplateLotSettingsBridge";
 import { installTaskTemplateProductAutofillBridge } from "./services/taskTemplateProductAutofillBridge";
 
 export * from "./domain/types";
@@ -9,4 +10,5 @@ export * from "./infrastructure/productCatalogRepository";
 installProductDrawerPricingBridge();
 installProductProcedureDisplayBridge();
 installTaskTemplateCocoAssistantBridge();
+installTaskTemplateLotSettingsBridge();
 installTaskTemplateProductAutofillBridge();
