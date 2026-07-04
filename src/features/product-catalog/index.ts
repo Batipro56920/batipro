e1deefe7525361d3d4ad2632e2c0a20296363689
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-04 11:14:22 +0200

    Enable product procedure display bridge

--- src/features/product-catalog/index.ts
+++ src/features/product-catalog/index.ts
@@ -1,8 +1,10 @@
 import { installProductDrawerPricingBridge } from "./services/productDrawerPricingBridge";
+import { installProductProcedureDisplayBridge } from "./services/productProcedureDisplayBridge";
 import { installTaskTemplateProductAutofillBridge } from "./services/taskTemplateProductAutofillBridge";
 
 export * from "./domain/types";
 export * from "./infrastructure/productCatalogRepository";
 
 installProductDrawerPricingBridge();
+installProductProcedureDisplayBridge();
 installTaskTemplateProductAutofillBridge();
