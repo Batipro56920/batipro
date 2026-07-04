091dd0ee7877338925f1d75b5383a6cda996ca8f
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-07-04 14:19:34 +0200

    Load task template lot dropdown bridge

--- src/features/product-catalog/index.ts
+++ src/features/product-catalog/index.ts
@@ -1,6 +1,7 @@
 import { installProductDrawerPricingBridge } from "./services/productDrawerPricingBridge";
 import { installProductProcedureDisplayBridge } from "./services/productProcedureDisplayBridge";
 import { installTaskTemplateCocoAssistantBridge } from "./services/taskTemplateCocoAssistantBridge";
+import { installTaskTemplateLotDropdownBridge } from "./services/taskTemplateLotDropdownBridge";
 import { installTaskTemplateLotSettingsBridge } from "./services/taskTemplateLotSettingsBridge";
 import { installTaskTemplateProductAutofillBridge } from "./services/taskTemplateProductAutofillBridge";
 
@@ -10,5 +11,6 @@ export * from "./infrastructure/productCatalogRepository";
 installProductDrawerPricingBridge();
 installProductProcedureDisplayBridge();
 installTaskTemplateCocoAssistantBridge();
+installTaskTemplateLotDropdownBridge();
 installTaskTemplateLotSettingsBridge();
 installTaskTemplateProductAutofillBridge();
