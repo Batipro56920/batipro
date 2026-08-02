9047f45aba4c04ee9316c04615c8595f92d6e587
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-08-03 00:16:27 +0200

    fix: reset Coco preparation feedback between templates

--- src/components/TaskTemplateDrawer.tsx
+++ src/components/TaskTemplateDrawer.tsx
@@ -324,6 +324,8 @@ export default function TaskTemplateDrawer({
     setPreparationSchemaReady(true);
     setPreparationError(null);
     setLocalError(null);
+    setCocoMessage(null);
+    setCocoError(null);
   }, [
     open,
     template?.id,
