f7bf9e4d2f74dcde788ba9bc35dae946ec065127
Batipro56920 <corentin.cbrenovation@gmail.com>
2026-08-23 10:43:18 +0200

    feat: enable direct intervenant access UX

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import "./compat/intervenantDirectAccessUx";
 
 declare global {
   interface Window {
@@ -74,5 +75,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-
